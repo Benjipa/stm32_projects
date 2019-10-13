b8853917a09abd7d910daed5cbd2ff50d5c4d74d
Benjamin <benji@LAPTOP-MHJ61MVE.localdomain>
2019-10-13 13:47:17 +0200

    first_version

---
+++ 1_test/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.d
@@ -0,0 +1,86 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
+ C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Inc/stm32f4xx_hal_conf.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Include/cmsis_version.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+
+C:/Users/benja/STM32_projekt/workspace_test/1_test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
