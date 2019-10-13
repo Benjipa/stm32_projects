9fc7c74daa12b571306563f57d759a48b75e1a97
Benjamin <benji@LAPTOP-MHJ61MVE.localdomain>
2019-10-13 14:02:03 +0200

    spi_master

---
+++ 1_spi_master/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.d
@@ -0,0 +1,98 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
+ C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Inc/stm32f4xx_hal_conf.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Include/cmsis_version.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+
+C:/Users/Benjamin/Arm_cortex_projekt/test1/workspace_test1/1_spi_master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
